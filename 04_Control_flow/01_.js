2025f656444b88f462fe7df02a60944f361f3bff
Harshit-0685 <harshitsinglaofficial23@gmail.com>
2024-07-15 03:12:07 +0530

    Commit #4

--- 04_Control_flow/01_.js
+++ 04_Control_flow/01_.js
@@ -1,8 +1,21 @@
 // if
 
-if(true){
-    console.log("Hello")
-}
+// if(true){
+//     console.log("Hello")
+// }
 
 // === - returns true when Both type and value are equal
 // !==  -returns true when type or value are not equal
+
+switch (2){
+    case 1:
+        console.log("Case 1 Executed");
+        break;
+    case 2:
+        console.log("Case 2 Executed")
+    case "third":
+        console.log("Case 3 Executed");
+    default:
+        console.log("Default Executed");
+        break;
+}
---
+++ 04_Control_flow/truthy.js
@@ -0,0 +1,33 @@
+const username = "H@rshit.io"
+
+if(username){
+    console.log(username);
+}
+else{
+    console.log("Provide a Username");
+}
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
+
+
+// Nullish Coalescing Operator (??): null undefined
+let val1 ;
+// val1 = 5 ?? 10;
+// val1 = null ?? 5;
+// val1 = undefined ?? 8;
+
+console.log(val1);
+
